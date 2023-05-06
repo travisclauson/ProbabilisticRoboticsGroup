d6c40d53b3221bc54543ebef2b605bf8d40f4e84
smspry <smspry@wpi.edu>
2023-05-05 21:19:45 -0400

    just added action space for robot

--- CS141 Final Project/Panda_Juggling/resources/robot.py
+++ CS141 Final Project/Panda_Juggling/resources/robot.py
@@ -1,7 +1,9 @@
 import pybullet as p
 import os
 import math
-
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
 
 class Robot:
     def __init__(self,client):
@@ -10,6 +12,7 @@ class Robot:
         startOrientation = p.getQuaternionFromEuler([0, 0, 0])
         f_name = os.path.join(os.path.dirname(__file__), 'panda_arm_hand.urdf')
         self.robot = p.loadURDF(f_name, startPos, startOrientation, useFixedBase = 1)
+        self.action_space = gym.spaces.Box(-1.0, 1.0, shape=(3), dtype=np.float32)      # 3 for x,y,z -- not doing joint action space yet might be too dificult
     
     def get_ids(self):
         return self.robot, self.client
